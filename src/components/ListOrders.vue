af1b67cf1c6ec726e06072c7e74ba1d02b64f150
Eugenio Navarro <navarro.mas.eugenio@gmail.com>
2020-02-17 18:48:59 +0100

    v-if update

--- src/components/ListOrders.vue
+++ src/components/ListOrders.vue
@@ -1,116 +1,137 @@
 <template>
-  <div class="order-list">
-    <h1>Orders List</h1>
-    <div class="order" v-for="order in list" v-bind:key="order.id">
-      <div class="order-header">
-        <span>Order: #{{ order.id }} - {{ order.date }}</span>
-        <!--<button v-on:click="updateOrder(item)">Update</button>-->
-        <button v-on:click="deleteOrder(order)">Delete</button>
-      </div>
-      <div class="order-item" v-for="item in order.list" v-bind:key="item.id">
-        <span>{{item.name}}</span>
-        <span>{{item.price}} €</span>
-        <span>x {{item.quantity}} =</span>
-        <span>{{ item.price * item.quantity }} €</span>
-      </div>
+    <div class="order-list">
+        <h1>Orders List</h1>
+        <div class="order" v-for="order in list" v-bind:key="order.id">
+            <div class="order-header">
+                <span>Order: #{{ order.id }} - {{ order.date }}</span>
+                <div>
+                    <button v-on:click="prepareUpdate(order.id)">Update</button>
+                    <button v-on:click="deleteOrder(order)">Delete</button>
+                </div>
+            </div>
+            <div class="order-item" v-for="item in order.list" v-bind:key="item.id">
+                <span>{{item.name}}</span>
+                <span>{{item.price}} €</span>
+                <span>x {{item.quantity}} =</span>
+                <span>{{ item.price * item.quantity }} €</span>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
-
 <script>
 import { EventBus } from "./event-bus.js";
 export default {
-  name: "ListOrders",
-  data: function() {
-    return {
-      list: null
-    };
-  },
+    name: "ListOrders",
+    data: function() {
+        return {
+            list: null,
+            send: null
+        };
+    },
 
-  methods: {
-    // leer todos los pedidos
-    readAll: function() {
-      fetch("http://localhost:3000/orders")
-        .then(response => response.json())
-        .then(json => (this.list = json));
-    },
-    // leer un pedido
-    readOrder: function(order) {
-      console.log(order.id);
-      fetch("http://localhost:3000/orders/" + order.id)
-        .then(response => response.json())
-        .then(json => console.log(json));
-    },
-    // añadir un pedido a la BD
-    createOrder: function(order) {
-      fetch("http://localhost:3000/orders", {
-        method: "POST",
-        body: JSON.stringify({
-          date: myDateString(),
-          list: order
-        }),
-        headers: {
-          "Content-type": "application/json; charset=UTF-8"
+    methods: {
+        // leer todos los pedidos
+        readAll: function() {
+            fetch("http://localhost:3000/orders")
+                .then(response => response.json())
+                .then(json => (this.list = json));
+        },
+        // leer un pedido
+        readOrder: function(id) {
+            fetch("http://localhost:3000/orders/" + id)
+                .then(response => response.json())
+                //.then(json => this.send = json)
+                .then(json => console.log(json));
+        },
+        // añadir un pedido a la BD
+        createOrder: function(order) {
+            fetch("http://localhost:3000/orders", {
+                    method: "POST",
+                    body: JSON.stringify({
+                        date: myDateString(),
+                        list: order
+                    }),
+                    headers: {
+                        "Content-type": "application/json; charset=UTF-8"
+                    }
+                })
+                //.then(response => response.json())
+                //.then(json => console.log(json))
+                .then(() => this.readAll());
+        },
+        // eliminar un pedido
+        deleteOrder: function(order) {
+            fetch("http://localhost:3000/orders/" + order.id, {
+                method: "DELETE"
+            }).then(() => this.readAll());
+        },
+        updateOrder: function(id, list) {
+            console.log(id); // MODIFICAR MÉTODO
+            console.log(list); // MODIFICAR MÉTODO
+        },
+        prepareUpdate: function(id) {
+            EventBus.$emit("change-update", id);
+            //EventBus.$emit("get-list", this.send);
         }
-      })
-        //.then(response => response.json())
-        //.then(json => console.log(json))
-        .then(() => this.readAll());
     },
-    // eliminar un pedido
-    deleteOrder: function(order) {
-      fetch("http://localhost:3000/orders/" + order.id, {
-        method: "DELETE"
-      }).then(() => this.readAll());
+    mounted: function() {
+        this.readAll();
     },
-    updateOrder: function(order) {
-      console.log(order.id);
+    // Escucha de ventos para crear y actualizar
+    created: function() {
+        EventBus.$on("create-order", order => {
+            this.createOrder(order);
+        });
+        EventBus.$on("update-order", (id, list) => {
+            this.updateOrder(id, list);
+        });
+        EventBus.$on("read-order", id => {
+            this.readOrder(id);
+            console.log(this.send);
+        });
     }
-  },
-  mounted: function() {
-    this.readAll();
-  },
-  created: function() {
-    EventBus.$on("create-order", order => {
-      this.createOrder(order);
-    });
-  }
 };
 
 function myDateString() {
-  let date = new Date();
-  let year = date.getFullYear();
-  let month = ("0" + (date.getMonth() + 1)).slice(-2);
-  let day = ("0" + date.getDate()).slice(-2);
-  let hours = ("0" + date.getHours()).slice(-2);
-  let min = ("0" + date.getMinutes()).slice(-2);
-  return day + "/" + month + "/" + year + " - " + hours + ":" + min;
+    let date = new Date();
+    let year = date.getFullYear();
+    let month = ("0" + (date.getMonth() + 1)).slice(-2);
+    let day = ("0" + date.getDate()).slice(-2);
+    let hours = ("0" + date.getHours()).slice(-2);
+    let min = ("0" + date.getMinutes()).slice(-2);
+    return day + "/" + month + "/" + year + " - " + hours + ":" + min;
 }
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .order-list {
-  flex-basis: 45%;
-  display: flex;
-  flex-direction: column;
-  align-content: flex-start;
+    flex-basis: 45%;
+    display: flex;
+    flex-direction: column;
+    align-content: flex-start;
+    background-color: lightgrey;
+    padding: 10px;
 }
 
 .order {
-  display: flex;
-  flex-direction: column;
+    display: flex;
+    flex-direction: column;
 }
 
-.order-header{
-  display: flex;
-  justify-content: space-between;
-  background-color: grey;
-  font-weight: bolder;
-  padding: 5px 10px;
+.order-header {
+    display: flex;
+    justify-content: space-between;
+    background-color: grey;
+    font-weight: bolder;
+    padding: 5px 10px;
 }
+
+.order-header button {
+    margin: 0 5px;
+}
+
 .order-item {
-  display: flex;
-  justify-content: space-evenly;
+    display: flex;
+    justify-content: space-evenly;
 }
 </style>
--- src/components/PrepareOrder.vue
+++ src/components/PrepareOrder.vue
@@ -1,129 +1,144 @@
 <template>
-  <div class="prepare-order">
-    <h1>Prepare Order</h1>
-    <div
-      class="card"
-      v-for="item in list"
-      v-bind:class="{minus10: item.minus10}"
-      v-bind:key="item.id"
-    >
-      <span>{{ item.name }}</span>
-      <div>
-        <span>{{ item.price }} € x</span>
-        <input v-model="item.quantity" type="number" min="1" max="99" />
-        <span>uds. = {{ item.price * item.quantity }} €</span>
-      </div>
-      <button v-on:click="deleteProduct(item)">Delete</button>
+    <div class="prepare-order">
+        <h1 v-if="prepare">Prepare Order</h1>
+        <h1 v-else>Update Order</h1>
+        <div class="card" v-for="item in list" v-bind:class="{minus10: item.minus10}" v-bind:key="item.id">
+            <span>{{ item.name }}</span>
+            <div>
+                <span>{{ item.price }} € x</span>
+                <input v-model="item.quantity" type="number" min="1" max="99" />
+                <span>uds. = {{ item.price * item.quantity }} €</span>
+            </div>
+            <button v-on:click="deleteProduct(item)">Delete</button>
+        </div>
+        <h2>Total: {{ calcTotal }} €</h2>
+        <div class="prepare-menu">
+            <label for="product">Product:</label>
+            <input type="text" v-model="inName" v-on:keyup.enter="addProduct" />
+            <label for="price">Price:</label>
+            <input type="text" v-model="inPrice" v-on:keyup.enter="addProduct" size="4" />€
+            <button v-on:click="addProduct">Add</button>
+            <button v-on:click="deleteLast">Delete last</button>
+        </div>
+        <br />
+        <button v-if="prepare" v-on:click="addOrder">Add Order</button>
+        <button v-else v-on:click="updateOrder">Update Order</button>
     </div>
-    <h2>Total: {{ calcTotal }} €</h2>
-    <div class="prepare-menu">
-      <label for="product">Product:</label>
-      <input type="text" v-model="inName" v-on:keyup.enter="addProduct" />
-      <label for="price">Price:</label>
-      <input type="text" v-model="inPrice" v-on:keyup.enter="addProduct" size="4" />€
-      <button v-on:click="addProduct">Add</button>
-      <button v-on:click="deleteLast">Delete last</button>
-    </div>
-    <br />
-    <button v-on:click="addOrder">Add Order</button>
-  </div>
 </template>
-
 <script>
 import { EventBus } from "./event-bus.js";
 export default {
-  name: "PrepareOrder",
-  data: function() {
-    return {
-      inName: "", //nombre del producto en el input
-      inPrice: 0, //precio del producto en el input
-      list: [] //lista de productos
-    };
-  },
-  computed: {
-    // calcula el precio total de la lista
-    calcTotal: function() {
-      let total = 0;
-      this.list.forEach(function(element) {
-        total += element.price * element.quantity;
-      });
-      return total;
-    }
-  },
-  methods: {
-    // añadir un producto a la lista
-    addProduct: function() {
-      if (Number(this.inPrice) > 0 && this.inName.trim()) {
-        this.list.push({
-          name: this.inName,
-          price: Number(this.inPrice),
-          quantity: 1,
-          minus10: Number(this.inPrice) < 10
+    name: "PrepareOrder",
+    data: function() {
+        return {
+            inName: "", //nombre del producto en el input
+            inPrice: 0, //precio del producto en el input
+            list: [], //lista de productos
+            id: null,
+            prepare: true
+        };
+    },
+    computed: {
+        // calcula el precio total de la lista
+        calcTotal: function() {
+            let total = 0;
+            this.list.forEach(function(element) {
+                total += element.price * element.quantity;
+            });
+            return total;
+        }
+    },
+    methods: {
+        // añadir un producto a la lista
+        addProduct: function() {
+            if (Number(this.inPrice) > 0 && this.inName.trim()) {
+                this.list.push({
+                    name: this.inName,
+                    price: Number(this.inPrice),
+                    quantity: 1,
+                    minus10: Number(this.inPrice) < 10
+                });
+            }
+            this.inName = "";
+            this.inPrice = 0;
+        },
+        // eliminar el último elemento de la lista
+        deleteLast: function() {
+            this.list.pop();
+        },
+        // eliminar un producto en particular
+        deleteProduct: function(product) {
+            this.list.splice(this.list.indexOf(product), 1);
+        },
+        addOrder: function() {
+            if (this.list.length > 0) {
+                EventBus.$emit("create-order", this.list);
+                this.list = [];
+            }
+        },
+        updateOrder: function() {
+            EventBus.$emit("update-order", this.id, this.list);
+            this.prepare = true;
+            this.list = [];
+        }
+    },
+    created: function() {
+        EventBus.$on("change-update", id => {
+            this.prepare = false;
+            this.id = id;
+            //EventBus.$emit("read-order", id);
         });
-      }
-      this.inName = "";
-      this.inPrice = 0;
-    },
-    // eliminar el último elemento de la lista
-    deleteLast: function() {
-      this.list.pop();
-    },
-    // eliminar un producto en particular
-    deleteProduct: function(product) {
-      this.list.splice(this.list.indexOf(product), 1);
-    },
-    addOrder: function() {
-      if (this.list.length > 0) {
-        EventBus.$emit("create-order", this.list);
-        this.list = [];
-      }
+        EventBus.$on("get-list", list => {
+            this.list.push(list);
+        });
     }
-  }
 };
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .prepare-order {
-  flex-basis: 50%;
-  display: flex;
-  flex-direction: column;
+    flex-basis: 50%;
+    display: flex;
+    flex-direction: column;
+    background-color: lightgrey;
+    padding: 10px;
 }
 
 .prepare-menu {
-  display: flex;
-  justify-content: space-evenly;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: baseline;
 }
 
 .card {
-  padding: 5px;
-  background-color: bisque;
-  border: 1px solid black;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
+    padding: 5px;
+    background-color: bisque;
+    border: 1px solid black;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
 }
 
-.card > span {
-  flex-basis: 30%;
+.card>span {
+    flex-basis: 30%;
 }
 
-.card > div {
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-  flex-basis: 60%;
+.card>div {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    flex-basis: 60%;
 }
 
-.card > div > * {
-  margin: 0 10px;
+.card>div>* {
+    margin: 0 10px;
 }
 
-.card > div > input {
-  width: 50px;
+.card>div>input {
+    width: 50px;
 }
 
 .minus10 {
-  color: red;
+    color: red;
 }
 </style>
